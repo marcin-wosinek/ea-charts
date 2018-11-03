cd497a48773e81567c0212fe2e6a64bfaefb9021
Marcin Wosinek <marcin.wosinek@gmail.com>
2018-11-03 17:22:29 +0100

    fix data splitting

---
+++ ea-survey/data/split.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+var fs = require('fs'),
+  d3Dsv = require('d3-dsv');
+
+var file = fs.readFileSync('./2018-ea-survey-anon-currencied-processed.csv', 'utf8');
+
+var data = d3Dsv.csvParse(file);
+
+data.columns.forEach((column) => {
+  fs.writeFileSync(column + '.csv', d3Dsv.csvFormat(data, [column]), 'utf8');
+});
