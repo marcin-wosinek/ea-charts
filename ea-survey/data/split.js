e45a980ff5606b037382977086a2cacd49da38fc
Marcin Wosinek <marcin.wosinek@gmail.com>
2018-11-03 18:18:04 +0100

    split data by prefix

--- ea-survey/data/split.js
+++ ea-survey/data/split.js
@@ -3,10 +3,24 @@
 var fs = require('fs'),
   d3Dsv = require('d3-dsv');
 
-var file = fs.readFileSync('./2018-ea-survey-anon-currencied-processed.csv', 'utf8');
+var file = fs.readFileSync(
+  './2018-ea-survey-anon-currencied-processed.csv',
+  'utf8',
+);
 
 var data = d3Dsv.csvParse(file);
 
-data.columns.forEach((column) => {
+['cause_import'].forEach(prefix => {
+  fs.writeFileSync(
+    prefix + '.csv',
+    d3Dsv.csvFormat(
+      data,
+      data.columns.filter(column => column.startsWith('cause_import')),
+    ),
+    'utf8',
+  );
+});
+
+data.columns.forEach(column => {
   fs.writeFileSync(column + '.csv', d3Dsv.csvFormat(data, [column]), 'utf8');
 });
