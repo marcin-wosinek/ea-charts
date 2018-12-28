8a7aba8dd8dd5699123c422cd9e4b7abfcff0391
Marcin Wosinek <marcin.wosinek@gmail.com>
2018-12-28 14:58:27 +0100

    add simple sub-id chart

--- ea-survey/data/process.js
+++ ea-survey/data/process.js
@@ -10,12 +10,23 @@ const _ = require('lodash'),
 const data = d3Dsv.csvParse(file);
 
 const processed = _.map(data, row => {
-  const subscribe = row.heard_ea === 'Yes' && row.is_ea1 === 'Yes';
+  const subscribe = row.heard_ea === 'Yes' && row.is_ea1 === 'Yes',
+    identifier = subscribe && row.is_ea2 === 'Yes',
+    subscriber = subscribe && row.is_ea2 === 'No';
+
+  let type;
+
+  if (identifier) {
+    type = 'identifier';
+  } else if(subscriber) {
+    type = 'subscriber';
+  } else {
+    type = 'none';
+  }
 
   return {
-    identifier: subscribe && row.is_ea2 === 'Yes',
-    subscriber: subscribe && row.is_ea2 === 'No',
-    country: row.country
+    country: row.country,
+    type
   };
 });
 
