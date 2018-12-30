eea30643dd8271f7067a15f09bf70f86a410df75
Marcin Wosinek <marcin.wosinek@gmail.com>
2018-12-30 12:20:27 +0100

    Add political data split

--- ea-survey/data/split.js
+++ ea-survey/data/split.js
@@ -1,39 +1,43 @@
 #!/usr/bin/env node
 
-var fs = require('fs'),
-  d3Dsv = require('d3-dsv');
+var fs = require("fs"),
+  d3Dsv = require("d3-dsv");
 
 var file = fs.readFileSync(
-  './2018-ea-survey-anon-currencied-processed.csv',
-  'utf8',
+  `${__dirname}/2018-ea-survey-anon-currencied-processed.csv`,
+  "utf8"
 );
 
 var data = d3Dsv.csvParse(file);
 
-['cause_import', 'ea_career'].forEach(prefix => {
+["cause_import", "ea_career"].forEach(prefix => {
   fs.writeFileSync(
-    prefix + '.csv',
+    `${__dirname}/${prefix}.csv`,
     d3Dsv.csvFormat(
       data,
-      data.columns.filter(column => column.startsWith(prefix)),
+      data.columns.filter(column => column.startsWith(prefix))
     ),
-    'utf8',
+    "utf8"
   );
 });
 
-const combinations = new Map([['engagement', ['want_ea_newsletter', 'which_year_EA', 'donate_2017_c']]]);
+const combinations = new Map([
+  ["engagement", ["want_ea_newsletter", "which_year_EA", "donate_2017_c"]],
+  ["political", ["which_year_EA", "donate_2017_c", "ea_career_type"]]
+]);
 
 for (let [key, value] of combinations) {
   fs.writeFileSync(
-    key + '.csv',
-    d3Dsv.csvFormat(
-      data,
-      value,
-    ),
-    'utf8',
+    `${__dirname}/${key}.csv`,
+    d3Dsv.csvFormat(data, value),
+    "utf8"
   );
 }
 
 data.columns.forEach(column => {
-  fs.writeFileSync(column + '.csv', d3Dsv.csvFormat(data, [column]), 'utf8');
+  fs.writeFileSync(
+    `${__dirname}/${column}.csv`,
+    d3Dsv.csvFormat(data, [column]),
+    "utf8"
+  );
 });
